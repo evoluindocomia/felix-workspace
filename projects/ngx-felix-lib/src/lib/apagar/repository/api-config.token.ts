b4562e4ba521d04ba1e201f4ab880d88d83fbd0b
Motivação Determinante - Aplicações <evoluindocomia@gmail.com>
2026-02-17 14:12:20 -0300

    limpeza e organização

--- projects/ngx-felix-lib/src/lib/apagar/repository/api-config.token.ts
+++
@@ -1,8 +0,0 @@
-﻿import { InjectionToken } from '@angular/core';
-
-export interface ApiConfig {
-  baseUrl: string;
-  responseAdapter?: (response: any) => any;
-}
-
-export const API_CONFIG = new InjectionToken<ApiConfig>('API_CONFIG');
--- projects/ngx-felix-lib/src/public-api.ts
+++ projects/ngx-felix-lib/src/public-api.ts
@@ -2,18 +2,6 @@
  * Public API Surface of ngx-felix-lib
  */
 
-// export * from './lib/ngx-felix-lib';
-// export * from './lib/apagar/repository/repository.interfaces';
-// export * from './lib/apagar/repository/query-builder';
-// export * from './lib/apagar/repository/api-config.token';
-// export * from './lib/apagar/repository/base-repository.service';
-
-// // MFE
-// export * from './lib/apagar/mfe/mfe.config';
-// export * from './lib/apagar/mfe/crypto.service';
-// export * from './lib/apagar/mfe/mfe-outlet.directive';
-// export * from './lib/apagar/mfe/mfe-receiver';
-
 // Exportações de Orquestração e MFE
 export * from './lib/mfe/mfe.interfaces';
 export * from './lib/mfe/mfe-outlet.directive';
